9709ee4338e564473f2d783073d1460c3a5a21e0
ahadk117 <134838283+ahadk117@users.noreply.github.com>
2024-09-18 14:21:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ echo "<hr>Enter data into the form to insert to database";
 
 <fieldset>
 <legend>Contact Form</legend>
-<a href="/test.php">test page redirect</a>
+<a href="phpwebapp/test.php">test page redirect</a>
 <form name="frmContact" method="post" action="index.php?send=1">
 <p>
 <label for="Name">Name </label>
